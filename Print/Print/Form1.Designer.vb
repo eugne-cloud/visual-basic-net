d8cf3c447f9b916e76175f0296255113e19674eb
Eugne Loh <82689492+eugne-cloud@users.noreply.github.com>
2021-05-18 20:26:15 +0800

    Add files via upload

---
+++ Print/Print/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPrintDoc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblIn = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblOut = New System.Windows.Forms.Label()
+        Me.txtOutput = New System.Windows.Forms.RichTextBox()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'lblIn
+        '
+        Me.lblIn.AutoSize = True
+        Me.lblIn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIn.Location = New System.Drawing.Point(51, 49)
+        Me.lblIn.Name = "lblIn"
+        Me.lblIn.Size = New System.Drawing.Size(51, 20)
+        Me.lblIn.TabIndex = 0
+        Me.lblIn.Text = "Input"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(115, 49)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(280, 26)
+        Me.txtInput.TabIndex = 1
+        '
+        'lblOut
+        '
+        Me.lblOut.AutoSize = True
+        Me.lblOut.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOut.Location = New System.Drawing.Point(51, 116)
+        Me.lblOut.Name = "lblOut"
+        Me.lblOut.Size = New System.Drawing.Size(64, 20)
+        Me.lblOut.TabIndex = 2
+        Me.lblOut.Text = "Output"
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(115, 116)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(280, 220)
+        Me.txtOutput.TabIndex = 3
+        Me.txtOutput.Text = ""
+        '
+        'btnWrite
+        '
+        Me.btnWrite.Location = New System.Drawing.Point(474, 49)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(116, 43)
+        Me.btnWrite.TabIndex = 4
+        Me.btnWrite.Text = "Write File"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(474, 116)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(116, 43)
+        Me.btnRead.TabIndex = 5
+        Me.btnRead.Text = "Read File"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(474, 182)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(116, 43)
+        Me.btnPrint.TabIndex = 6
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'frmPrintDoc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.lblOut)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblIn)
+        Me.Name = "frmPrintDoc"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblIn As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblOut As Label
+    Friend WithEvents txtOutput As RichTextBox
+    Friend WithEvents btnWrite As Button
+    Friend WithEvents btnRead As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+End Class
---
+++ Print/Print/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class frmPrintDoc
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        Dim FILE_NAME As String = "C:\Users\Acer\Desktop\D-CS 2020\JAN2020_LECTURES\VB.NET\Print"
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+            Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+
+            objWriter.Write(txtInput.Text)
+            objWriter.Close()
+            MessageBox.Show("Text Written to File")
+        Else
+            MessageBox.Show("File Does Not Exist!")
+        End If
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        Dim FILE_NAME As String = "C:\Users\Acer\Desktop\D-CS 2020\JAN2020_LECTURES\VB.NET\Print\Test.txt"
+
+        Dim TextLine As String
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+            Dim objReader As New System.IO.StreamReader(FILE_NAME)
+
+            Do While objReader.Peek() <> -1
+                TextLine = TextLine & objReader.ReadLine() & vbNewLine
+            Loop
+
+            txtOutput.Text = TextLine
+        Else
+            MessageBox.Show("File Does Not Exist!!")
+        End If
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        PrintDialog1.Document = PrintDocument1
+        PrintDialog1.PrinterSettings = PrintDocument1.PrinterSettings
+        PrintDialog1.AllowSomePages = True
+
+        Try
+            If PrintDialog1.ShowDialog = DialogResult.OK Then
+                PrintDocument1.PrinterSettings = PrintDialog1.PrinterSettings
+                PrintDocument1.Print()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+    End Sub
+End Class
