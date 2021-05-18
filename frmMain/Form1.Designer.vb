208750590b5ff810d17a82a48614ec52eacb2027
Eugne Loh <82689492+eugne-cloud@users.noreply.github.com>
2021-05-18 20:28:40 +0800

    Add files via upload

---
+++ frmMain/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnOn = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtTime = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnOn
+        '
+        Me.btnOn.Image = CType(resources.GetObject("btnOn.Image"), System.Drawing.Image)
+        Me.btnOn.Location = New System.Drawing.Point(67, 131)
+        Me.btnOn.Name = "btnOn"
+        Me.btnOn.Size = New System.Drawing.Size(256, 257)
+        Me.btnOn.TabIndex = 0
+        Me.btnOn.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Image = CType(resources.GetObject("btnStop.Image"), System.Drawing.Image)
+        Me.btnStop.Location = New System.Drawing.Point(457, 131)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(272, 257)
+        Me.btnStop.TabIndex = 1
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(67, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(137, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter A Number"
+        '
+        'txtTime
+        '
+        Me.txtTime.Location = New System.Drawing.Point(222, 54)
+        Me.txtTime.Name = "txtTime"
+        Me.txtTime.Size = New System.Drawing.Size(100, 26)
+        Me.txtTime.TabIndex = 4
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtTime)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnOn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOn As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtTime As TextBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ frmMain/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        txtTime.Text = txtTime.Text + 1
+    End Sub
+
+    Private Sub btnOn_Click(sender As Object, e As EventArgs) Handles btnOn.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        Timer1.Stop()
+    End Sub
+End Class
